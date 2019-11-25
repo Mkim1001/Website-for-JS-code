78a7d294eb11767fa8e0d5e02e4618c89751a7a0
Mkim1001 <54555662+Mkim1001@users.noreply.github.com>
2019-11-25 15:05:18 -0800

    Add files via upload

---
+++ sketch-f4.js
@@ -0,0 +1,24 @@
+let counter = 0;
+
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  let angle = counter;
+  
+  let x = counter;
+  let y = sin(angle*0.1)* 100;
+  y+y + 200;
+  
+  if (x > width)(counter = 0)
+  
+  fill("red")
+  ellipse(x, y, 50)
+
+counter++
+}
